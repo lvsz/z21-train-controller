4dd01f5deb83ad44333486e7262fcd62b2fea146
lvsz <levisiuzdak@gmail.com>
2022-03-19 10:50:33 +0100

    Improved pqueue and mheap datastructures
    The end user of a pqueue no longer has to keep track of item indices
    themselves through callback functions.

--- src/data/pqueue.rkt
+++ src/data/pqueue.rkt
@@ -10,54 +10,55 @@
 
 (require "mheap.rkt")
 
-(struct pqueue (mheap))
+(struct pqueue (mheap indices))
 
 (struct item (value (priority #:mutable)) #:transparent)
 
-(define (pair->item x)
-  (item (car x) (cdr x)))
+;(define (pq-notify notify)
+;  (lambda (idx item)
+;    (notify idx (item-value item) (item-priority item))))
 
-(define (on accessor cmp)
-  (lambda (x y)
-    (cmp (accessor x)
-         (accessor y))))
+(define (pq-notify pq)
+  (lambda (idx val)
+    (hash-set! (pqueue-indices pq) (item-value val) idx)))
 
-(define (pqueue-notify notify)
-  (lambda (i item)
-    (notify i (item-value item) (item-priority item))))
-
-(define (make-pqueue <<?)
-  (pqueue (make-mheap (on item-priority <<?))))
+(define (make-pqueue compare)
+  (pqueue (make-mheap (lambda (x y)
+                        (compare (item-priority x) (item-priority y))))
+          (make-hash)))
 
 (define (pqueue-empty? pq)
   (mheap-empty? (pqueue-mheap pq)))
 
-(define (pqueue-enqueue! pq val priority notify)
-  (mheap-insert! (pqueue-mheap pq) (item val priority) (pqueue-notify notify)))
+(define (pqueue-enqueue! pq val priority)
+  (mheap-insert! (pqueue-mheap pq) (item val priority) (pq-notify pq)))
 
-(define (pqueue-serve! pq notify)
+(define (pqueue-serve! pq)
   (when (pqueue-empty? pq)
     (error "pqueue-serve!: empty pqueue" pq))
-  (let ((pq-item (mheap-delete! (pqueue-mheap pq) (pqueue-notify notify))))
+  (let ((pq-item (mheap-delete! (pqueue-mheap pq) (pq-notify pq))))
+    (hash-remove! (pqueue-indices pq) (item-value pq-item))
     (values (item-value pq-item) (item-priority pq-item))))
 
-(define (pqueue-reschedule! pq i new-priority notify)
-  (let ((pq-item (mheap-peek-at (pqueue-mheap pq) i)))
+(define (pqueue-reschedule! pq val new-priority)
+  (let* ((idx (hash-ref (pqueue-indices pq) val))
+         (pq-item (mheap-peek-at (pqueue-mheap pq) idx)))
     (set-item-priority! pq-item new-priority)
-    (mheap-touch-at! (pqueue-mheap pq) i (pqueue-notify notify))))
+    (mheap-touch-at! (pqueue-mheap pq) idx (pq-notify pq))))
 
 (define (pqueue-peek pq)
   (when (pqueue-empty? pq)
     (error "pqueue-peek: empty pqueue" pq))
   (item-value (mheap-peek (pqueue-mheap pq))))
 
-(define (pqueue-peek-at pq i)
+(define (pqueue-peek-at pq idx)
   (when (pqueue-empty? pq)
     (error "pqueue-peek-at: empty pqueue" pq))
-  (item-value (mheap-peek-at (pqueue-mheap pq) i)))
+  (item-value (mheap-peek-at (pqueue-mheap pq) idx)))
 
-(define (pqueue-priority-of pq i)
+(define (pqueue-priority-of pq val)
   (when (pqueue-empty? pq)
     (error "pqueue-peek-at: empty pqueue" pq))
-  (item-priority (mheap-peek-at (pqueue-mheap pq) i)))
+  (item-priority (mheap-peek-at (pqueue-mheap pq)
+                                (hash-ref (pqueue-indices pq) val))))
 
