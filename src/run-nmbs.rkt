122ac1fe8609521cf443217910f0628390a649ea
lvsz <levisiuzdak@gmail.com>
2022-03-20 16:25:22 +0100

    Files to run client & server

--- src/run-nmbs.rkt
+++ src/run-nmbs.rkt
@@ -1,18 +1,25 @@
 #lang racket/base
 
+(provide run-nmbs)
+
 (require racket/class
          "nmbs/nmbs.rkt"
-         "infrabel/infrabel.rkt")
+         "infrabel/infrabel.rkt"
+         (prefix-in tcp: "infrabel/client.rkt")
+         (prefix-in local: "infrabel/infrabel.rkt"))
 
-(define (main)
+(define (run-nmbs (remote #t))
 
   (define infrabel
-    (new infrabel%))
+    (if remote
+      (new tcp:infrabel%)
+      (new local:infrabel%)))
 
   (define nmbs
     (new nmbs% (infrabel infrabel)))
 
   (send nmbs start))
 
-(main)
+(module* main #f
+  (void (run-nmbs)))
 
---
+++ src/run-server.rkt
@@ -0,0 +1,7 @@
+#lang racket/base
+
+(require "infrabel/server.rkt")
+
+(module* main #f
+  (void (start-server)))
+
