6f7c264957df80e4ffec4146fcb456393c2a14c7
lvsz <levisiuzdak@gmail.com>
2022-03-20 19:40:43 +0100

    Added command-line arguments to run-nmbs

--- src/run-nmbs.rkt
+++ src/run-nmbs.rkt
@@ -8,7 +8,22 @@
          (prefix-in tcp: "infrabel/client.rkt")
          (prefix-in raw: "infrabel/infrabel.rkt"))
 
-(define (run-nmbs #:remote? (remote #t))
+
+;; The starts up the program.
+;;   if #:remote? = #f : everything runs in a single instance
+;;   if #:remote? = #t : it needs to connect with a server
+;;     the server can be either localhost or raspberypi
+;;   if #:setup = #f : gives the user a selection screen to pick a setup
+;;   otherwise it will skip that step (assuming the given setup exists)
+(define (run-nmbs #:remote? (remote #f) #:setup (setup #f))
+  (for ((arg (in-vector (current-command-line-arguments))))
+    (case arg
+      (("--hardware" "-h")
+       (set! setup 'hardware))
+      (("--remote" "-r")
+       (set! remote #t))
+      (else
+       (eprintf "Unrecognized argument: ~a~%" arg))))
 
   (define infrabel
     (if remote
@@ -18,8 +33,10 @@
   (define nmbs
     (new nmbs% (infrabel infrabel)))
 
-  (send nmbs start))
+  (send nmbs start setup))
 
+
+;; Doesn't run when imported as a module elsewhere
 (module* main #f
   (void (run-nmbs)))
 
