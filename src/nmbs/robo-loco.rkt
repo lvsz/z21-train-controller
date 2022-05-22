e68e68ba8892b76b530f6b6bbb06647f470024f5
lvsz <levisiuzdak@gmail.com>
2022-05-22 22:18:00 +0200

    Added some documentation

--- src/nmbs/robo-loco.rkt
+++ src/nmbs/robo-loco.rkt
@@ -61,51 +61,57 @@
     (define (choo-choo pos)
       (let ((lt (send loco get-location))
             (db (send nmbs get-loco-d-block loco-id)))
-        (cond ((and (>= pos (sub1 route-length))
-                    (eq? lt (vector-ref route (sub1 route-length))))
-               ; we're finished
-               (update-loco (send lt get-id)
-                            (send (vector-ref route (- route-length 2))
-                                  get-id))
-               #t)
-              ((not (d-block? (curr pos)))
-               ; just passed a d-block, time to switch switches
-               ; will return through mutual recursion
-               (set-switches pos))
-              (db ; loco is on a d-block
-                (if (eq? db (send lt get-id))
-                  (begin
-                    (when (and (pair? reversals)
-                               (eq? lt (car reversals)))
-                      (sleep sleep-time)
-                      (set! reversals (cdr reversals))
-                      (send nmbs change-loco-direction loco-id))
-                    (let wait ((db (send nmbs get-loco-d-block loco-id)))
-                      ; wait in a loop until loco shows up on a d-block
-                      (sleep sleep-time)
-                      (cond ((not db)
-                             ; not on a d-block yes, wait longer
-                             (wait (send nmbs get-loco-d-block loco-id)))
-                            ((eq? db (send (curr pos) get-id))
-                             ; good d-block found; continue
-                             (update-loco (send (curr pos) get-id)
-                                          (send (prev pos) get-id))
-                             (choo-choo (add1 pos)))
-                            ((not (memq db (map (lambda (x) (send x get-id))
-                                                route-list)))
-                             ; check if loco is still on planned route
-                             (update-loco db)
-                             #f)
-                            (else
-                             (wait (send nmbs get-loco-d-block loco-id))))))
-                  (let ((id-vec (vector-map (lambda (x) (send x get-id)) route)))
-                    (update-loco db)
-                    ; check loco is still on the planned route
-                    (match (vector-memq db id-vec)
-                      (#f #f)
-                      (n  (choo-choo (add1 n)))))))
-              (else (sleep sleep-time)
-                    (choo-choo pos)))))
+        (cond
+          ; opt 1: we're finished
+          ((and (>= pos (sub1 route-length))
+                (eq? lt (vector-ref route (sub1 route-length))))
+           (update-loco (send lt get-id)
+                        (send (vector-ref route (- route-length 2))
+                              get-id))
+           #t)
+          ; opt 2: just passed a d-block, time to switch switches
+          ((not (d-block? (curr pos)))
+           ; returns thru mutual recursion
+           (set-switches pos))
+          ; opt 3: loco is on a d-block
+          (db
+            (if (eq? db (send lt get-id))
+              ; opt 3a: track given by nmbs matches loco's last known location
+              (begin
+                (when (and (pair? reversals)
+                           (eq? lt (car reversals)))
+                  (sleep sleep-time)
+                  (set! reversals (cdr reversals))
+                  (send nmbs change-loco-direction loco-id))
+                (let wait ((db (send nmbs get-loco-d-block loco-id)))
+                  ; wait in a loop until loco shows up on a d-block
+                  (sleep sleep-time)
+                  (cond ((not db)
+                         ; not on a d-block yes, wait longer
+                         (wait (send nmbs get-loco-d-block loco-id)))
+                        ((eq? db (send (curr pos) get-id))
+                         ; good d-block found; continue
+                         (update-loco (send (curr pos) get-id)
+                                      (send (prev pos) get-id))
+                         (choo-choo (add1 pos)))
+                        ((not (memq db (map (lambda (x) (send x get-id))
+                                            route-list)))
+                         ; check if loco is still on planned route
+                         (update-loco db)
+                         #f)
+                        (else
+                          (wait (send nmbs get-loco-d-block loco-id))))))
+              ; opt 3b: no match, update location and check if still on route
+              (let ((id-vec (vector-map (lambda (x) (send x get-id)) route)))
+                (update-loco db)
+                ; check loco is still on the planned route
+                (match (vector-memq db id-vec)
+                  (#f #f)
+                  (n  (choo-choo (add1 n)))))))
+          ; opt 4: wait & see
+          (else
+            (sleep sleep-time)
+            (choo-choo pos)))))
 
     (define/public (start)
       (thread (lambda ()
