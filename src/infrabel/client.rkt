b23dab317e710a9545cf26c4a94aad89a6a4971d
lvsz <levisiuzdak@gmail.com>
2022-05-29 16:56:37 +0200

    Added unique tags to TCP messages

--- src/infrabel/client.rkt
+++ src/infrabel/client.rkt
@@ -4,9 +4,22 @@
 
 (require racket/tcp
          racket/class
+         racket/match
          "interface.rkt"
          "../logger.rkt")
 
+
+;; Logging function that can be enabled
+(define log/i void)
+(define log/d void)
+
+(define tcp-files (directory-list "resources/tcp/" #:build? #t))
+
+
+;; Struct for sending a request over TCP
+;; body contains the arguments
+;; on-response is either #f or a function that takes the response as argument
+(struct request (body response) #:constructor-name new-request)
 
 ;; Read TCP port & host from file
 (define (tcp-info file)
@@ -17,98 +30,107 @@
               (read-line in)))))
 
 
-;; TCP input & output
-(define-values (in out)
-  (values #f #f))
-
-
 ;; Quickly try going through the different configs available
-(define tcp-files (directory-list "resources/tcp/" #:build? #t))
 (define (quick-connect (files tcp-files))
-  (if (null? files)
-    (begin (eprintf "tcp-connect failed~%")
-           (exit))
-    (let-values (((port host) (tcp-info (car files))))
-      (with-handlers ((exn:fail:network?
-                        (lambda (exn)
-                          (eprintf "tcp-connect on ~a@~a failed~%" port host)
-                          (quick-connect (cdr files)))))
-                     ; connection succesful, update i/o ports
-                     (let-values (((i o) (tcp-connect host port)))
-                       (set! in i)
-                       (set! out o))))))
-
-
-;; Struct for sending a request over TCP
-;; msg contains the request
-;; on-response is either #f or a function that takes the response as argument
-(struct request (msg on-response))
-
-
-;; Several threads may be sending & requesting data over tcp,
-;; so use this thread to avoid race conditions
-(define communicator
-  (thread (lambda ()
-            (let loop ()
-              ; blocks till thread receives a request
-              (let* ((req (thread-receive))
-                     (msg (request-msg req))
-                     (on-response (request-on-response req)))
-                (log/d (format "requesting ~a" msg))
-                (writeln msg out)
-                (flush-output out)
-                (if on-response
-                  (let ((response (read in)))
-                    (unless (eof-object? response)
-                      (log/d (format "response for ~a: ~a" msg response))
-                      (on-response response)
-                      (loop)))
-                  (loop)))))))
-
-
-;; Send a message over TCP
-(define (put . args)
-  (thread-send communicator
-               (request args #f)))
-
-
-;; Request something over tcp
-(define (get . args)
-  (let ((response #f)
-        (responded #f))
-  (thread-send
-    communicator
-    (request args (lambda (x) (set! response x) (set! responded #t))))
-  (let wait ()
-    (if responded
-      response
-      (begin (sleep 0.1)
-             (wait))))))
-
-
-;; Logging function that can be enabled
-(define log/i void)
-(define log/d void)
+  (let/cc
+    return
+    (if (null? files)
+      (begin (eprintf "tcp-connect failed~%")
+             (exit))
+      (let-values (((port host) (tcp-info (car files))))
+        (with-handlers
+          ((exn:fail:network?
+             (lambda (exn)
+               (begin0 (quick-connect (cdr files))
+                       (eprintf "tcp-connect on ~a@~a failed~%" port host)))))
+          ; connection succesful, update i/o ports
+          (let-values (((in out) (tcp-connect host port)))
+            (return in out)))))))
 
 
 ;; For interchangeability purposes, this has the exact same interface
 ;; as the infrabel% class in infrabel.rkt
 (define infrabel-client%
   (class* object% (infrabel-interface<%>)
-    (init-field (log-level 'debug))
     (super-new)
+    (init (log-level 'debug))
 
     (when log-level
       (set!-values (log/i log/d) (make-loggers 'infrabel/client)))
 
+    (define-values (tcp-in tcp-out)
+      (quick-connect))
+
+    (define loco-speed-listeners (make-hash))
+    (define switch-listeners (make-hash))
+
+    ;; Several threads may be sending & requesting data over tcp,
+    ;; the main purpose of this thread is to keep them organised
+    (define (client-loop)
+      (define pending (make-hash))
+      (define (update args)
+        (log/d "Received update:" args)
+        (case (car args)
+          ((loco-speed)
+           (for ((proc (in-hash-keys loco-speed-listeners)))
+             (apply proc (cdr args))))
+          ((switch)
+           (for ((proc (in-hash-keys switch-listeners)))
+             (apply proc (cdr args))))
+          (else
+           (match (hash-ref pending (car args) #f)
+             (#f (log/i "Cannot not recognize received update:" args))
+             (fn (fn (cdr args))
+                 (hash-remove! pending (car args)))))))
+      (define (request req)
+        (let ((msg (request-body req))
+              (response (request-response req))
+              (id (string->symbol (symbol->string (gensym))))) ; force intern
+          (log/d "Received request" msg)
+          (write (cons id msg) tcp-out)
+          (flush-output tcp-out)
+          (when response
+            (hash-set! pending id response))))
+      (let loop ()
+        (match (sync (choice-evt tcp-in (thread-receive-evt)))
+          ((? input-port?) (update (read tcp-in)))
+          (_ (request (thread-receive))))
+        (log/d (hash-count pending) "pending responses.")
+        (loop)))
+
+    (define client-thread
+      (thread client-loop))
+
+    ;; Any message to the server should be sent using this function
+    (define (communicate msg (callback #f))
+      (thread-send client-thread
+                   (new-request msg callback)))
+
+    ;; Send a message over TCP
+    (define (put . args)
+      (communicate args))
+
+    ;; Request something over tcp
+    (define (get . args)
+      (define (respond-to ch)
+        (lambda (r)
+          ;(unless (eof-object? response)
+          (channel-put ch r)))
+      (let ((ch (make-channel)))
+        (communicate args (respond-to ch))
+        (let ((response (channel-get ch)))
+          (log/d "Response:" response)
+          response)))
+
+
     (define/public (initialize setup-id (mode 'sim))
       (log/i "Initializing")
-      (quick-connect)
       (put 'initialize setup-id mode))
 
     (define/public (initialized?)
       (get 'initialized?))
 
+    ;; Server already starts infrabel
     (define/public (start)
       (void))
     (define/public (stop)
