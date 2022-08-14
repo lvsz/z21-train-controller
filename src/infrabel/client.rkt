b2b09c7ff39145bcc0189356baaf3e2a21f0c7f6
lvsz <levisiuzdak@gmail.com>
2022-08-14 18:09:15 +0200

    Improved message handling, can forward `'kill`

--- src/infrabel/client.rkt
+++ src/infrabel/client.rkt
@@ -18,9 +18,9 @@
 
 
 ;; Struct that facilitates sending requests in need of a response
-;; header contains the method name
-;; body contains the arguments
-;; on-response is either #f or a function that takes the response as argument
+;; `header` contains the method name
+;; `body` contains the arguments
+;; `on-response` is either #f or a function that takes the response as argument
 (struct request (header body response) #:constructor-name new-request)
 
 ;; Read TCP port & host from file
@@ -37,29 +37,20 @@
   (let/cc
     return
     (if (null? files)
-      (begin (eprintf "tcp-connect failed~%")
+      (begin (log/w "Failed to establish TCP connection")
+             (eprintf "tcp-connect failed~%")
              (exit))
       (let-values (((port host) (tcp-info (car files))))
         (with-handlers
-          ((exn:fail:network?
+          ((exn:fail:network? ; Call the following if connection failed
              (lambda (exn)
-               (begin0 (quick-connect (cdr files))
-                       (eprintf "tcp-connect on ~a@~a failed~%" port host)))))
+               (log/i (format "TCP connection to ~a@~a failed" port host))
+               (quick-connect (cdr files)))))
+          (log/i (format "Attempting TCP connection to ~a@~a" port host))
           ; Connection succesful, update i/o ports
           (let-values (((in out) (tcp-connect host port)))
             (file-stream-buffer-mode out 'none)
             (return in out)))))))
-
-
-;; Returns a unique id generator for messages
-(define (id-gen)
-  (let ((n 0))
-    (lambda ()
-      (set! n (+ n 1))
-      n)))
-
-;; Check whether a value could be a request id
-(define request-id? integer?)
 
 
 ;; For interchangeability purposes, this has the exact same interface
@@ -77,24 +68,30 @@
     (define/public (get-update)
       update-channel)
 
-    ; `id-gen` returns a lambda to generate ids
-    (define gen-id (id-gen))
+    ; Generate new identifiers using a closure
+    (define gen-id (let ((n 0))
+                     (lambda () (set! n (add1 n)) n)))
 
     ; Several threads may be sending & requesting data over TCP
     ; The main purpose of this thread is to keep them organised
     (define (client-loop)
       (define pending (make-hash))
-      (define (update msg)
+      (define (handle-message msg)
         (log/d "Received update:" msg)
-        ; If id doesn't fit that of a request, forward to update-channel
-        ; Otherwise try to fill in pending request
-        (if (not (request-id? (message-id msg)))
-          (send-update (cons (message-id msg) (message-body msg)))
-          (let ((fn (hash-ref pending (message-id msg) #f)))
-            (if fn
-              (begin (fn (message-body msg))
-                     (hash-remove! pending (message-id msg)))
-              (log/w "Cannot not recognize received update:" msg)))))
+        (match msg
+          ; Response to a pending request
+          ((message id 'response body)
+           ; Try to retrieve the pending callback function, call it with `body`
+           ((hash-ref pending id (lambda () (log/w msg "not in" pending))) body)
+           (hash-remove! pending id))
+          ; State update from infrabel
+          ((message id 'update body)
+           (send-update (cons id body)))
+          ; Server severed connection
+          ((or (? eof-object?) (message _ 'kill _))
+           (log/w "Disconnected from server with message:" msg)
+           (send-update '(kill)))
+          (_ (log/w "Cannot not recognize received update:" msg))))
       (define (request req)
         (let* ((id (gen-id))
                (msg (message id (request-header req) (request-body req))))
@@ -102,28 +99,34 @@
           (write msg tcp-out)
           (when (request-response req)
             (hash-set! pending id (request-response req)))))
-      (let loop ()
-        (match (sync (choice-evt tcp-in (thread-receive-evt)))
-          ; When a TCP port synchronizes, it returns itself
-          ((? input-port?) (update (read tcp-in)))
-          ; Otherwise assume it's a thread message
-          (_ (request (thread-receive))))
-        (log/d "Pending responses:" pending)
-        (loop)))
+      (with-handlers
+        ((exn:fail:network? (lambda (exn)
+                              (log/w "Network error:" exn)
+                              (handle-message eof))))
+        ; Looping part of `client-loop`
+        (let loop ()
+          ; Blocks until server sent something or thread received something
+          (match (sync (choice-evt tcp-in (thread-receive-evt)))
+            ; When a TCP port synchronizes, it returns itself
+            ((? input-port?) (handle-message (read tcp-in)))
+            ; Otherwise assume it's a thread message
+            (_ (request (thread-receive))))
+          (log/d "Pending responses:" pending)
+          (loop))))
 
     (define client-thread
       (thread client-loop))
 
-    ;; Any message to the server should be sent using this function
+    ; Any message to the server should be sent using this function
     (define (communicate header body (callback #f))
       (thread-send client-thread
                    (new-request header body callback)))
 
-    ;; Send a message over TCP
+    ; Send a message over TCP
     (define (put header . body)
       (communicate header body))
 
-    ;; Request something over TCP
+    ; Request something over TCP
     (define (get header . body)
       (define (respond-to ch)
         (lambda (r)
